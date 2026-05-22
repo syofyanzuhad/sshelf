220bbbe0f278c220618b8364267b022cb7e77676
Syofyan Zuhad <mail@syofyanzuhad.dev>
2026-05-23 02:12:47 +0700

    fix: make database seeder idempotent and set admin plan to business

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Enums\Plan;
+use App\Enums\UserRole;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
@@ -30,6 +32,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'role' => UserRole::Viewer,
+            'plan' => Plan::Free,
         ];
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Enums\Plan;
+use App\Enums\UserRole;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +20,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Admin',
-            'email' => 'mail@syofyanzuhad.dev',
-        ]);
+        User::updateOrCreate(
+            ['email' => 'mail@syofyanzuhad.dev'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('password'),
+                'role' => UserRole::Admin,
+                'plan' => Plan::Business,
+                'email_verified_at' => now(),
+            ]
+        );
     }
 }
