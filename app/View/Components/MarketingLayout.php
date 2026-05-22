3a03e5b544def07c87852990fba9d904bd1b65db
Syofyan Zuhad <mail@syofyanzuhad.dev>
2026-05-22 10:05:18 +0700

    fix: add MarketingLayout component class to resolve x-marketing-layout

---
+++ app/View/Components/MarketingLayout.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+use Illuminate\View\View;
+
+class MarketingLayout extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(
+        public ?string $title = null,
+        public ?string $description = null,
+    ) {}
+
+    /**
+     * Get the view / contents that represents the component.
+     */
+    public function render(): View
+    {
+        return view('layouts.marketing');
+    }
+}
