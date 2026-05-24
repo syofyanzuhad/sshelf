d468b62bbe6b2764e64aa678f60a6e7d802263ff
Syofyan Zuhad 🇵🇸 <mail@syofyanzuhad.dev>
2026-05-24 15:41:52 +0700

    Remove version prefix from API route group

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Api\V1\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
