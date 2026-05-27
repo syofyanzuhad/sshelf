2056bf0b2e044218e921188e6364ce18783df209
Syofyan Zuhad <mail@syofyanzuhad.dev>
2026-05-27 23:25:41 +0700

    feat: integrate Reverb monitoring cards into Pulse dashboard and move Telescope to core dependencies

--- config/pulse.php
+++ config/pulse.php
@@ -3,6 +3,8 @@
 use Laravel\Pulse\Http\Middleware\Authorize;
 use Laravel\Pulse\Pulse;
 use Laravel\Pulse\Recorders;
+use Laravel\Reverb\Pulse\Recorders\ReverbConnections;
+use Laravel\Reverb\Pulse\Recorders\ReverbMessages;
 
 return [
 
@@ -137,6 +139,14 @@ return [
     */
 
     'recorders' => [
+        ReverbConnections::class => [
+            'sample_rate' => 1,
+        ],
+    
+        ReverbMessages::class => [
+            'sample_rate' => 1,
+        ],
+
         Recorders\CacheInteractions::class => [
             'enabled' => env('PULSE_CACHE_INTERACTIONS_ENABLED', true),
             'sample_rate' => env('PULSE_CACHE_INTERACTIONS_SAMPLE_RATE', 1),
