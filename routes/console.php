407e24dabbd7d9093423f20f806502e858b704d9
Syofyan Zuhad <mail@syofyanzuhad.dev>
2026-07-31 16:17:58 +0700

    Change telescope prune schedule to run hourly

--- routes/console.php
+++ routes/console.php
@@ -20,4 +20,4 @@ Schedule::call(function () {
         ]);
 })->hourly();
 
-Schedule::command('telescope:prune', ['--hours' => 48])->daily();
+Schedule::command('telescope:prune', ['--hours' => 48])->hourly();
